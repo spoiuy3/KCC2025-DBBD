ae96b9a138907398a868fa7ae172536957a0119f
Spoiuy3 <spoiuy3@khu.ac.kr>
2025-03-24 15:55:46 +0900

    Edit: .

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ from ssg.checkpoints import CheckpointIO
 import cProfile
 import matplotlib
 import torch_geometric
-import torch.multiprocessing as mp
-mp.set_start_method('spawn', force=True)
 
 # disable GUI
 matplotlib.pyplot.switch_backend('agg')
